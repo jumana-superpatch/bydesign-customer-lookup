9dcd2a41ebe46c35804348e11f35adf8669d0243
jumana-superpatch <jumanal@superpatch.com>
2025-09-15 14:34:54 -0400

    tes t

--- src/index.js
+++ src/index.js
@@ -188,7 +188,7 @@ async function findCustomerInShopify(email, shop, token) {
 }
 
 async function findCustomerInByDesign(email, base, apiKey) {
-  const res = await fetch(`${base}/VoxxLifeSandbox/api/users/customer/CustomerLookup`, {
+  const res = await fetch(`${base}/VoxxLife/api/users/customer/CustomerLookup`, {
     method: "POST",
     headers: {
       Accept: "application/json",
